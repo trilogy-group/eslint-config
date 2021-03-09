33de1934f44c9946a329c0a0db3ecdc774655949
Robert Egglestone <robert.egglestone@trilogy.com>
2021-03-09 13:27:14 +1300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ module.exports = {
     extends: [
         'standard-with-typescript'
     ],
-    rules: {}
+    rules: {
+        '@typescript-eslint/ban-types': 'error',
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+        '@typescript-eslint/no-explicit-any': 'error',
+        'consistent-return': 'error',
+        'no-empty-function': 'error',
+        'no-func-assign': 'error',
+        'no-shadow': 'error',
+        'no-unused-labels': 'error'
+    }
 }
